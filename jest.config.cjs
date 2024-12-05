9cc3e13ea133916fc5b03c1125cd5caa06bab6ed
Essi Lähteenmäki <lahteenmakiessi@hotmail.fi>
2024-12-05 20:23:32 +0200

    Test coveralls

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,4 +3,31 @@ module.exports = {
     '^.+\\.jsx?$': 'babel-jest', 
   },
   testEnvironment: 'node', 
+  collectCoverage: true,
+
+  collectCoverageFrom: [
+    "src/**/*.{js}", // Include all source files
+    "!src/**/*.test.{js}", // Exclude test files
+  ],
+
+  // Output format
+  coverageReporters: [
+    "text", // Text report shown in the terminal
+    "lcov", // LCOV format (used by Coveralls)
+    "json-summary", // JSON summary (optional)
+  ],
+
+  coverageDirectory: "coverage",
+
+  // Threshold testing
+  /*
+  coverageThreshold: {
+    global: {
+      branches: 80,   // Minimum branch coverage
+      functions: 80,  // Minimum function coverage
+      lines: 80,      // Minimum line coverage
+      statements: 80, // Minimum statement coverage
+    },
+  },
+  */
 };
